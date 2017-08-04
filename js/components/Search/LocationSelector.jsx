d75e03e58008e6dab9c85e5b3ffa0892cacb3d0c
Julie Pan <ycp217@nyu.edu>
2017-08-04 18:15:34 -0400

    add city and state selector

--- js/components/Search/LocationSelector.jsx
+++ js/components/Search/LocationSelector.jsx
@@ -4,6 +4,7 @@ import * as api from 'actions/api';
 // import withRouter from 'react-router/lib/withRouter';
 import Select from 'react-select';
 // import {searchConstant} from './constants';
+import states from './states';
 
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
@@ -32,65 +33,139 @@ const styles = {
   },
 };
 
-const loadCountries = debounce((value, cb) => {
-  return api.get(`/database-contacts/locations?country=${value}`)
-  .then(response => {
-    console.log(response);
-    cb(null, {options: response.data.map(({countryName}) => ({value: countryName}))});
-  });
-}, 750);
+
+class Selector extends Component {
+  constructor(props) {
+    super(props);
+    this.onCountryChange = obj => this.props.onLocationSelect({country: !!obj ? obj.value : undefined, state: this.props.state, city: this.props.city});
+    this.onStateChange = obj => this.props.onLocationSelect({country: this.props.country, state: !!obj ? obj.value : undefined, city: this.props.city});
+    this.onCityChange = obj => this.props.onLocationSelect({country: this.props.country, state: this.props.state, city: !!obj ? obj.value : undefined});
+    this.loadCountries = debounce(this.loadCountries.bind(this), 750);
+    this.loadStates = debounce(this.loadStates.bind(this), 750);
+    this.loadCities = debounce(this.loadCities.bind(this), 750);
+  }
+
+  loadCountries(country, cb) {
+    cb(null, {options: [{value: 'United States'}]});
+    // if (country.length > 0) {
+    //   return api.get(`/database-contacts/locations?country=${country}`)
+    //   .then(response => {
+    //     console.log(response);
+    //     cb(null, {options: response.data.map(({countryName}) => ({value: countryName}))});
+    //   });
+    // }
+  }
+
+  loadStates(state, cb) {
+    cb(null, {options: states});
+    // if (state.length > 0) {
+    //   return api.get(`/database-contacts/locations?country=${this.props.country}&state=${state}`)
+    //   .then(response => {
+    //     console.log(response);
+    //     cb(null, {options: response.data.map(({stateName}) => ({value: stateName}))});
+    //   });
+    // }
+  }
+
+  loadCities(city, cb) {
+    if (city.length > 0) {
+      return api.get(`/database-contacts/locations?country=${this.props.country}&state=${this.props.state}&city=${city}`)
+      .then(response => {
+        // console.log(response);
+        cb(null, {options: response.data.map(({cityName}) => ({value: cityName}))});
+      });
+    }
+  }
+
+  render() {
+    const {index, country, state, city} = this.props;
+    return (
+      <div style={styles.selectContainer} >
+        <div className='vertical-center'>
+          <div style={styles.select} >
+            <Select.Async
+            placeholder='Country'
+            labelKey='value'
+            onChange={this.onCountryChange}
+            value={country}
+            loadOptions={this.loadCountries}
+            />
+          </div>
+          <div style={styles.select} >
+            <Select.Async
+            placeholder='State'
+            labelKey='value'
+            onChange={this.onStateChange}
+            value={state}
+            loadOptions={this.loadStates}
+            />
+          </div>
+          <div style={styles.select} >
+            <Select.Async
+            placeholder='City'
+            labelKey='value'
+            onChange={this.onCityChange}
+            value={city}
+            loadOptions={this.loadCities}
+            />
+          </div>
+        {index > 0 &&
+          <div style={{margin: '0 10px'}} >
+            <IconButton
+            tooltip='Remove'
+            tooltipPosition='top-right'
+            style={styles.small}
+            iconStyle={Object.assign({}, styles.smallIcon, {color: grey600})}
+            iconClassName='fa fa-times'
+            onClick={this.props.onLocationDelete}
+            />
+          </div>}
+        </div>
+      </div>
+      );
+  }
+}
 
 class LocationSelector extends Component {
   constructor(props) {
     super(props);
+    this.loadCountries = debounce(this.loadCountries.bind(this), 750);
+    this.loadStates = debounce(this.loadStates.bind(this), 750);
+    this.loadCities = debounce(this.loadCities.bind(this), 750);
+  }
+
+  loadCountries(country, cb) {
+    if (country.length > 0) {
+      return api.get(`/database-contacts/locations?country=${country}`)
+      .then(response => {
+        console.log(response);
+        cb(null, {options: response.data.map(({countryName}) => ({value: countryName}))});
+      });
+    }
+  }
+
+  loadStates(state, cb) {
+
+  }
+
+  loadCities(city, cb) {
+
   }
 
   render() {
     return (
       <div>
-      {this.props.locations.map(({country, state, city}, i) =>
-        <div style={styles.selectContainer} key={`select-${i}`} >
-          <div className='vertical-center'>
-            <div style={styles.select} >
-              <Select.Async
-              placeholder='Country'
-              labelKey='value'
-              onChange={obj => this.props.onLocationSelect(i, {country: !!obj ? obj.value : undefined, state, city})}
-              value={country}
-              loadOptions={loadCountries}
-              />
-            </div>
-            <div style={styles.select} >
-              <Select
-              placeholder='State'
-              labelKey='value'
-              onChange={obj => this.props.onLocationSelect(i, {country, state: !!obj ? obj.value : undefined, city})}
-              value={state}
-              options={[{value: 'New York'}]}
-              />
-            </div>
-            <div style={styles.select} >
-              <Select
-              placeholder='City'
-              labelKey='value'
-              onChange={obj => this.props.onLocationSelect(i, {country, state, city: !!obj ? obj.value : undefined})}
-              value={city}
-              options={[{value: 'Boston'}]}
-              />
-            </div>
-          {i > 0 &&
-            <div style={{margin: '0 10px'}} >
-              <IconButton
-              tooltip='Remove'
-              tooltipPosition='top-right'
-              style={styles.small}
-              iconStyle={Object.assign({}, styles.smallIcon, {color: grey600})}
-              iconClassName='fa fa-times'
-              onClick={_ => this.props.onLocationDelete(i)}
-              />
-            </div>}
-          </div>
-        </div>
+      {this.props.locations
+        .map(({country, state, city}, i) =>
+          <Selector
+          key={`location-selector-set-${i}`}
+          index={i}
+          country={country}
+          state={state}
+          city={city}
+          onLocationDelete={_ => this.props.onLocationDelete(i)}
+          onLocationSelect={obj => this.props.onLocationSelect(i, obj)}
+          />
         )}
         <FlatButton label='Add Location' icon={<FontIcon className='fa fa-plus' />} onClick={this.props.onLocationAdd} />
       </div>
---
+++ js/components/Search/states.js
@@ -0,0 +1,240 @@
+const states = [
+  {
+    'value': 'Alabama',
+    'abbreviation': 'AL'
+  },
+  {
+    'value': 'Alaska',
+    'abbreviation': 'AK'
+  },
+  {
+    'value': 'American Samoa',
+    'abbreviation': 'AS'
+  },
+  {
+    'value': 'Arizona',
+    'abbreviation': 'AZ'
+  },
+  {
+    'value': 'Arkansas',
+    'abbreviation': 'AR'
+  },
+  {
+    'value': 'California',
+    'abbreviation': 'CA'
+  },
+  {
+    'value': 'Colorado',
+    'abbreviation': 'CO'
+  },
+  {
+    'value': 'Connecticut',
+    'abbreviation': 'CT'
+  },
+  {
+    'value': 'Delaware',
+    'abbreviation': 'DE'
+  },
+  {
+    'value': 'District Of Columbia',
+    'abbreviation': 'DC'
+  },
+  {
+    'value': 'Federated States Of Micronesia',
+    'abbreviation': 'FM'
+  },
+  {
+    'value': 'Florida',
+    'abbreviation': 'FL'
+  },
+  {
+    'value': 'Georgia',
+    'abbreviation': 'GA'
+  },
+  {
+    'value': 'Guam',
+    'abbreviation': 'GU'
+  },
+  {
+    'value': 'Hawaii',
+    'abbreviation': 'HI'
+  },
+  {
+    'value': 'Idaho',
+    'abbreviation': 'ID'
+  },
+  {
+    'value': 'Illinois',
+    'abbreviation': 'IL'
+  },
+  {
+    'value': 'Indiana',
+    'abbreviation': 'IN'
+  },
+  {
+    'value': 'Iowa',
+    'abbreviation': 'IA'
+  },
+  {
+    'value': 'Kansas',
+    'abbreviation': 'KS'
+  },
+  {
+    'value': 'Kentucky',
+    'abbreviation': 'KY'
+  },
+  {
+    'value': 'Louisiana',
+    'abbreviation': 'LA'
+  },
+  {
+    'value': 'Maine',
+    'abbreviation': 'ME'
+  },
+  {
+    'value': 'Marshall Islands',
+    'abbreviation': 'MH'
+  },
+  {
+    'value': 'Maryland',
+    'abbreviation': 'MD'
+  },
+  {
+    'value': 'Massachusetts',
+    'abbreviation': 'MA'
+  },
+  {
+    'value': 'Michigan',
+    'abbreviation': 'MI'
+  },
+  {
+    'value': 'Minnesota',
+    'abbreviation': 'MN'
+  },
+  {
+    'value': 'Mississippi',
+    'abbreviation': 'MS'
+  },
+  {
+    'value': 'Missouri',
+    'abbreviation': 'MO'
+  },
+  {
+    'value': 'Montana',
+    'abbreviation': 'MT'
+  },
+  {
+    'value': 'Nebraska',
+    'abbreviation': 'NE'
+  },
+  {
+    'value': 'Nevada',
+    'abbreviation': 'NV'
+  },
+  {
+    'value': 'New Hampshire',
+    'abbreviation': 'NH'
+  },
+  {
+    'value': 'New Jersey',
+    'abbreviation': 'NJ'
+  },
+  {
+    'value': 'New Mexico',
+    'abbreviation': 'NM'
+  },
+  {
+    'value': 'New York',
+    'abbreviation': 'NY'
+  },
+  {
+    'value': 'North Carolina',
+    'abbreviation': 'NC'
+  },
+  {
+    'value': 'North Dakota',
+    'abbreviation': 'ND'
+  },
+  {
+    'value': 'Northern Mariana Islands',
+    'abbreviation': 'MP'
+  },
+  {
+    'value': 'Ohio',
+    'abbreviation': 'OH'
+  },
+  {
+    'value': 'Oklahoma',
+    'abbreviation': 'OK'
+  },
+  {
+    'value': 'Oregon',
+    'abbreviation': 'OR'
+  },
+  {
+    'value': 'Palau',
+    'abbreviation': 'PW'
+  },
+  {
+    'value': 'Pennsylvania',
+    'abbreviation': 'PA'
+  },
+  {
+    'value': 'Puerto Rico',
+    'abbreviation': 'PR'
+  },
+  {
+    'value': 'Rhode Island',
+    'abbreviation': 'RI'
+  },
+  {
+    'value': 'South Carolina',
+    'abbreviation': 'SC'
+  },
+  {
+    'value': 'South Dakota',
+    'abbreviation': 'SD'
+  },
+  {
+    'value': 'Tennessee',
+    'abbreviation': 'TN'
+  },
+  {
+    'value': 'Texas',
+    'abbreviation': 'TX'
+  },
+  {
+    'value': 'Utah',
+    'abbreviation': 'UT'
+  },
+  {
+    'value': 'Vermont',
+    'abbreviation': 'VT'
+  },
+  {
+    'value': 'Virgin Islands',
+    'abbreviation': 'VI'
+  },
+  {
+    'value': 'Virginia',
+    'abbreviation': 'VA'
+  },
+  {
+    'value': 'Washington',
+    'abbreviation': 'WA'
+  },
+  {
+    'value': 'West Virginia',
+    'abbreviation': 'WV'
+  },
+  {
+    'value': 'Wisconsin',
+    'abbreviation': 'WI'
+  },
+  {
+    'value': 'Wyoming',
+    'abbreviation': 'WY'
+  }
+];
+
+export default states;
